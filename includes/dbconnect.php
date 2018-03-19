804f68ddbf920d5d515b6bddc10403d2c7b2352e
surojmaharjan <surojmaharjan@gmail.com>
2018-03-19 15:25:14 -0400

    Update dbconnect.php

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 
 // CONNECT TO THE DATABASE
-$DB_NAME = 'opsworks_book_demo';
+$DB_NAME = 'napalayatravels';//'opsworks_book_demo';
 $DB_HOST = '172.31.17.39';//'172.31.87.176'; // CHANGE THIS TO YOUR OPSWORKS MYSQL SERVER PRIVATE IP ADDRESS
 $DB_USER = 'root'; // KEEP THIS IN PLACE AS PER CHAPTER 4 
 $DB_PASS = 'luldi2hp56'; //'8ePO2O8E0UHG91975I3k'; // KEEP THIS IN PLACE AS PER CHAPTER 4
